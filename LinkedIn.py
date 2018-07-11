34507f8a4867ea4d35e92600c6d9df1b38e69b5e
AndreasMilesUK <AndreasMilesUK@users.noreply.github.com>
2018-07-11 03:09:42 +0100

    FuckYeah

--- LinkedIn.py
+++ LinkedIn.py
@@ -8,29 +8,41 @@ import csv
 
 companies = 'C:\Companies.xlsx'
 companies = pd.read_excel(companies,
-                         sheet_name='Global',           #2nd sheet of the workbook
+                         sheet_name='Global',           
                           header=0,
-                          index_col= 2,
+                          index_col= 3,
                           names=None,
                           converters=None,
                           dtype=None,
                           keep_default_na=True)
 
-comp_list = companies["Company Name"].tolist()
-print(comp_list)
+comp_list = companies["Exchange:Ticker"].tolist()
+tickers = []
+
+for ticker in comp_list:
+    try:      
+        code = ticker.split(':')[1]
+        tickers.append(code)
+    except:
+        pass
+
+#comp_list = []
+#for name in comp:
+#    l = name.rfind('(')
+#    comp_list.append(name[:l-1])
 
 
 names = []
-for list in comp_list:
-    url = 'https://www.google.com/search?q=' + list + ' LinkedIn' + ' HR Manager UK'
+for list in tickers:
+    url = 'https://www.reuters.com/finance/stocks/company-officers/'+list
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text)
-    listc = str.lower(list.split(' ', 1)[0])
+    #listc = str.lower(list.split(' ', 1)[0])
     for link in soup.find_all('a'):
         title = link.text
-        if listc.title() in title or listc in title :
-            names.append(title)
+        if searchword in title and 'linkedin' in title:
+            names.append(title,link)
 
 with open('Names.txt','w') as f:
     f.write(','.join(names))
